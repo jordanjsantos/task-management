7c4309a58105900ff68b4cb22512f2adbc8e58fa
Jordan Santos <jrdnsantos@gmail.com>
2026-05-21 23:14:58 -0300

    feat: add persistence layer and TaskDbContext

---
+++ backend/TaskManagement.Api/TaskManagement.Api/Persistence/TaskDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagement.Api.Persistence
+{
+    public class TaskDbContext(DbContextOptions<TaskDbContext> options) : DbContext(options)
+    {
+        public DbSet<Entities.Task> Tasks { get; set; }
+    }
+}
--- backend/TaskManagement.Api/TaskManagement.Api/Program.cs
+++ backend/TaskManagement.Api/TaskManagement.Api/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Api.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TaskDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
