3b427029b37c7ac05f8ae1c938d40c1bfe3a9999
Jordan Santos <jrdnsantos@gmail.com>
2026-05-22 00:41:29 -0300

    feat: add find all tasks

---
+++ backend/TaskManagement.Api/TaskManagement.Api/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using TaskManagement.Api.Entities;
+using TaskManagement.Api.Persistence;
+
+namespace TaskManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskDbContext _dbContext;
+
+        public TaskController(TaskDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Entities.Task>>> FindAll()
+        {
+            var tasks = await _dbContext.Tasks.ToListAsync();
+            return Ok(tasks);
+        }
+    }
+}
--- backend/TaskManagement.Api/TaskManagement.Api/Entities/Task.cs
+++ backend/TaskManagement.Api/TaskManagement.Api/Entities/Task.cs
@@ -6,6 +6,6 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Status { get; set; }
-        public DateTime CreationTime { get; set; }
+        public DateTime CreationTime { get; set; } = DateTime.Now;
     }
 }
